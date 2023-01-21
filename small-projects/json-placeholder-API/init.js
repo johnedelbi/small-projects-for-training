9966b78df88ca221950ad60047597abe30701143
John Edelbi <info@jedelbi.com>
2023-01-21 23:32:37 +0100

    add some change to the structure

---
+++ small-projects/json-placeholder-API/init.js
@@ -0,0 +1,6 @@
+import { getAllUsersListener } from "./listeners/getAllUsersListener.js"
+import { getSingleUserListener } from "./listeners/getSingleUserListener.js"
+
+
+getAllUsersListener();
+getSingleUserListener();
